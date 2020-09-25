c657a6e2b524660547f0c40d13fcb84f48e3ac6d
dgarrimar <dgarrimar@gmail.com>
2020-09-25 11:50:08 +0200

    add benchmark(2) scripts

---
+++ benchmark2.nf
@@ -0,0 +1,283 @@
+/*
+ * Asymptotic distribution of Anderson's test statistic in complex models
+ * Benchmarking setting 
+ * Diego Garrido Martín 
+ */
+
+
+/*
+ *  Define parameters
+ */
+
+// General params
+params.dir = 'result/bm'
+params.out = 'benchmark'
+params.help = false
+
+// Simulation params
+params.a = 2
+params.b = 3
+params.n = 100
+params.u = 1
+params.q = 3
+params.t = 'none'
+params.which = 'B'
+params.model = 'Y~A+B+AB.bm2.R'
+params.gen = 'mvnorm'
+params.sim = 1000000
+params.k = 100
+params.delta = 0
+params.hk = 1
+
+// Generation: multivariate normal
+params.y_var = 'equal'
+params.y_cor = 0
+
+// Generation: copula
+params.c_dist = 'unif-0-1'
+
+// Generation: simplex (proportions)
+params.p_loc = 1
+params.p_sd = 0.1
+params.p_dist = 'norm'
+
+// Generation: multinomial
+params.lambda = 1000
+
+// Model: Y~C+A.R
+params.r = 0
+params.C_mean = 0
+params.C_var = 1
+
+
+/*
+ *  Print usage and help
+ */
+
+if (params.help) {
+  log.info ''
+  log.info 'Asymptotic Distribution of Anderson\'s Test Statistic in Complex Models'
+  log.info '======================================================================='
+  log.info 'Simulations given a set of parameters'
+  log.info ''
+  log.info 'Usage: '
+  log.info '    nextflow run simulate.nf [options]'
+  log.info ''
+  log.info 'General parameters:'
+  log.info ' --a LEVELS_A                levels of factor A (default: 2)'
+  log.info ' --b LEVELS_B                levels of factor B (default: 3)'
+  log.info ' --n SAMPLE_SIZE             total number of samples (default: 100)'
+  log.info ' --u UNBALANCE               unbalance, 1 is balanced. (default: 1)'
+  log.info ' --q RESPONSES               number of response variables (default: 3)'
+  log.info ' --t TRANSFORM               transform response variables: none, sqrt or log (default: none)'
+  log.info ' --which WHICH               which factor changes in H1: A, B or AB (default: B)'
+  log.info ' --model MODEL               R script with model definition (default: Y~A+B+AB.R)'
+  log.info ' --gen GENERATION            data generation: mvnorm, simplex, multinom or copula (default: mvnorm)'
+  log.info ' --sim SIMULATIONS           number of simulations (default: 1000000)'
+  log.info ' --k CHUNKS                  number of chunks (default: 100)'
+  log.info ' --delta DELTA               change in H1 (default: 0.1)'
+  log.info ' --hk HETEROSKEDASTICITY     heteroskedasticity, 1 is homoskedastic (default: 1)'
+  log.info ' --dir DIRECTORY             output directory (default: result)'
+  log.info ' --out OUTPUT                output file (default: simulation.tsv)'
+  log.info ''
+  log.info 'Additional parameters for generation = mvnorm:'
+  log.info ' --y_var VARIANCE            variance of response variables: equal or unequal (default: equal)'
+  log.info ' --y_cor CORRELATION         correlation of response variables (default: 0)'
+  log.info ''
+  log.info 'Additional parameters for generation = copula:'
+  log.info ' --y_var VARIANCE            variance of response variables: equal or unequal (default: equal)'
+  log.info ' --y_cor CORRELATION         correlation of response variables (default: 0)'
+  log.info ' --c_dist DISTRIBUTION       multivariate non-normal distribution definition (default: unif-0-1)'
+  log.info ''
+  log.info 'Additional parameters for generation = simplex:'
+  log.info ' --p_loc LOCATION            location in the simplex of the generator model, 1 is centered (default: 1)'
+  log.info ' --p_sd STDEV                standard deviation of the generator model (default: 0.1)'
+  log.info ' --p_dist DISTRIBUTION       distribution of the generator model (default: norm)'
+  log.info ''
+  log.info 'Additional parameters for generation = multinom:'
+  log.info ' --lambda LAMBDA             lambda parameter (Poisson distribution) to generate multinomial distribution\'s size parameter (default: 1000)'
+  log.info ''
+  log.info 'Additional parameters for model = Y~C+A.R'
+  log.info ' --r CORRELATION             (if generation = simplex: amount of noise added to the) correlation between C and Y1 (default: 0)'
+  log.info ' --C_mean MEAN               mean of covariate C (default: 0)'
+  log.info ' --C_var VARIANCE            variance of covariate C (default: 1)'
+  log.info ''
+  exit(1)
+}
+
+
+/*
+ *  Print parameter selection
+ */
+
+log.info ''
+log.info 'SIMULATION SCHEMA'
+log.info ''
+log.info 'General parameters'
+log.info '------------------'
+log.info "Levels of factor A           : ${params.a}"
+log.info "Levels of factor B           : ${params.b}"
+log.info "Total sample size            : ${params.n}"
+log.info "Unbalance degree             : ${params.u}"
+log.info "Number of Y variables        : ${params.q}"
+log.info "Transformation of Y          : ${params.t}"
+log.info "Which factor changes in H1   : ${params.which}"
+log.info "Model definition             : ${params.model}"
+log.info "Data generation              : ${params.gen}"
+log.info "Number of simulations        : ${params.sim}"
+log.info "Number of chunks             : ${params.k}"
+log.info "Change in H1 (delta)         : ${params.delta}"
+log.info "Heteroskedasticity           : ${params.hk}"
+log.info "Output directory             : ${params.dir}"
+log.info "Output file                  : ${params.out}"
+log.info ''
+if(params.gen == 'mvnorm'){
+  log.info 'Additional parameters'
+  log.info '---------------------'
+  log.info "Variance of Y variables      : ${params.y_var}"
+  log.info "Correlation of Y variables   : ${params.y_cor}"
+} else if (params.gen == 'copula'){
+  log.info 'Additional parameters'
+  log.info '---------------------'
+  log.info "Variance of Y variables      : ${params.y_var}"
+  log.info "Correlation of Y variables   : ${params.y_cor}"
+  log.info "Distribution definition      : ${params.c_dist}"
+} else if (params.gen == 'simplex'){
+  log.info 'Additional parameters'
+  log.info '---------------------'
+  log.info "Location in the simplex      : ${params.p_loc}"
+  log.info "Generation stdev             : ${params.p_sd}"
+  log.info "Generation distribution      : ${params.p_dist}"
+} else if (params.gen == 'multinom'){
+  log.info 'Additional parameters'
+  log.info '---------------------'
+  log.info "Lambda                       : ${params.lambda}"
+} else if (params.model == 'Y~C+A.R'){
+  if(params.gen == 'simplex'){
+    log.info "(Noise added to) cor(C, Y1)  : ${params.r}"
+  }
+  log.info "cor(C, Y[,1])                : ${params.r}"
+  log.info "Mean of covariate C          : ${params.C_mean}"
+  log.info "Variance of covariate C      : ${params.C_var}"
+}
+log.info ''
+
+
+/*
+ *  Expand parameters
+ */
+
+def grid = [:]
+params.keySet().each{
+  if(it in ['a','b','n','u','q','delta','hk','y_var','y_cor','p_loc','p_sd','p_dist','c_dist','lambda','r','C_mean','C_var']){
+    grid[it] = params[it]
+  }
+}
+
+grid.keySet().each {
+    if (grid[it] =~ /,/){
+        grid[it] = grid[it].tokenize(',')
+    } else if (grid[it] =~ /:/) {
+        def (start, end, step) = grid[it].tokenize(':')
+        def seq = []
+        def val = start.toFloat()
+        while(val.round(4) <= end.toFloat().round(4)){
+            seq += val.round(4)
+            val += step.toFloat().round(4)
+        }
+        grid[it] = seq
+    }
+}
+
+
+/*
+ *  Simulate
+ */
+
+process getEmpirical {
+ 
+    input:
+    each a from grid.a
+    each b from grid.b
+    each n from grid.n
+    each u from grid.u
+    each q from grid.q
+    each d from grid.delta
+    each hk from grid.hk
+    each y_var from grid.y_var
+    each y_cor from grid.y_cor
+    each p_loc from grid.p_loc
+    each p_sd from grid.p_sd
+    each p_dist from grid.p_dist      
+    each lambda from grid.lambda
+    each c_dist from grid.c_dist
+    each r from grid.r
+    each C_mean from grid.C_mean
+    each C_var from grid.C_var
+    each chunk from Channel.from(1..params.k)
+
+    output:
+    
+    file('empirical.txt') into emp_ch
+
+    script:
+    """
+    s=\$(echo ${params.sim}/${params.k} | R --vanilla --quiet | sed -n '2s/.* //p')
+    ${params.model} -a $a -b $b -n $n -u $u -q $q -d $d -H $hk -v $y_var -c $y_cor -p $p_loc -s $p_sd --p_dist $p_dist -l $lambda -D $c_dist -r $r --C_mean $C_mean --C_var $C_var -S \$s -x 10 -k $chunk -m ${params.gen} -t ${params.t} -w ${params.which} -o empirical.txt
+    """
+}
+  
+process getProposed {
+
+    clusterOptions="-pe smp 10"
+ 
+    input:
+    each a from grid.a
+    each b from grid.b
+    each n from grid.n
+    each u from grid.u
+    each q from grid.q
+    each d from grid.delta
+    each hk from grid.hk
+    each y_var from grid.y_var
+    each y_cor from grid.y_cor
+    each p_loc from grid.p_loc
+    each p_sd from grid.p_sd
+    each p_dist from grid.p_dist
+    each lambda from grid.lambda
+    each c_dist from grid.c_dist
+    each r from grid.r
+    each C_mean from grid.C_mean
+    each C_var from grid.C_var
+ 
+    output:
+    file("${params.out}.RData") into perm_ch
+
+    script:
+    """
+    ${params.model} -a $a -b $b -n $n -u $u -q $q -d $d -H $hk -v $y_var -c $y_cor -p $p_loc -s $p_sd --p_dist $p_dist -l $lambda -D $c_dist -r $r --C_mean $C_mean --C_var $C_var -S ${params.sim} -x 10 -k 0 -m ${params.gen} -t ${params.t} -w ${params.which} -o ${params.out}
+    """
+}
+
+emp_ch
+.collectFile(name: "${params.out}.tsv")
+.set{emp_merged_ch}
+
+process end {
+
+   publishDir "${params.dir}"     
+
+   input:
+   file(emp) from emp_merged_ch
+   file(perm) from perm_ch
+
+   output:
+   set file(emp), file(perm) into end_ch
+
+   script:
+   """
+   echo "DONE"
+   """
+}
+
+
---
+++ bin/Y~A+B+AB.bm2.R
@@ -0,0 +1,300 @@
+#!/usr/bin/Rscript
+
+## Evaluation of asymptotic Anderson's test in complex models
+## Model: Y ~ A + B + AB
+
+##  0. Parse arguments
+
+library(optparse)
+
+
+option_list = list(
+  make_option(c("-q", "--q"), type="numeric", default=3,
+              help="Number of dependent variables [default %default]", metavar="numeric"),
+  make_option(c("-a","--a_levels"), type="numeric", default=2,
+              help="Number of levels of factor A [default %default]", metavar="numeric"),
+  make_option(c("-b","--b_levels"), type="numeric", default=3,
+              help="Number of levels of factor B [default %default]", metavar="numeric"),
+  make_option(c("-r", "--C_cor"), type="numeric", default=0,
+              help="(If generator = simplex: noise added to the) correlation of covariate C with Y[,1] [default %default]", metavar="numeric"),
+  make_option(c("--C_mean"), type="numeric", default=0,
+              help="Mean of covariate C [default %default]", metavar="numeric"),
+  make_option(c("--C_var"), type="numeric", default=1,
+              help="Variance of covariate C [default %default]", metavar="numeric"),
+  make_option(c("-u","--unbalance"), type="numeric", default=1,
+              help="Unbalance degree (level 1) [default %default]", metavar="numeric"),
+  make_option(c("-n","--no_samples"), type="numeric", default=100,
+              help="Total number of samples [default %default]", metavar="numeric"),
+  make_option(c("-S","--simulations"), type="numeric", default=1e3,
+              help="Number of simulations [default %default]", metavar="numeric"),
+  make_option(c("-m", "--model"), type="character", default="mvnorm",
+              help="H0/H1 generator model: 'mvnorm', 'simplex' or 'multinom' [default %default]",
+              metavar="character"),
+  make_option(c("-d","--delta"), type="numeric", default=0,
+              help="H1 generation parameter [default %default]", metavar="numeric"),
+  make_option(c("-c","--correlation_y"), type="numeric", default=0,
+              help="Correlation of the Y variables [default %default]", metavar="numeric"),
+  make_option(c("-w","--which"), type="character", default = "B",
+              help="Which factor changes: A, B or AB", metavar = "character"),
+  make_option(c("-v","--variance_y"), type="character", default="equal",
+              help="Variance of the Y variables: 'equal' or 'unequal' [default %default]", metavar="character"),
+  make_option(c("-s","--stdev"), type="numeric", default=0.1,
+              help="stdev for the 'simplex' generator model [default %default]", metavar="numeric"),
+  make_option(c("-p","--position"), type="numeric", default=1,
+              help="location of the 'simplex' generator model [default %default]", 
+              metavar="numeric"),
+  make_option(c("--p_dist"), type="character", default="norm",
+              help="Distribution for 'simplex' generator model: norm, gamma or beta [default %default]", 
+              metavar="character"),
+  make_option(c("-D","--DistDef"), type="character", default="unif-0-1",
+              help="Multivariate non-normal distribution definition [default %default]", 
+              metavar="character"),
+  make_option(c("-l","--lambda"), type="numeric", default=1000,
+              help="lambda parameter (Poisson distribution) to generate size for 'multinom' generator model [default %default]", 
+              metavar="numeric"),
+  make_option(c("-H","--heterosk"), type="numeric", default= 1,
+              help="Heteoskedasticity degree (level 1) [default %default]", metavar="numeric"),
+  make_option(c("-t","--transf"), type="character", default="none",
+              help="Data transformation: sqrt or None [default %default]", metavar="character"),
+  make_option(c("-k","--k_chunk"), type="numeric", default=0,
+              help="Chunk number [default %default]", metavar="numeric"),
+  make_option(c("-x","--cpu"), type="numeric", default=10,
+              help="Number of cores [default %default]", metavar="numeric"),
+  make_option(c("-o", "--output"), type="character", default=NULL,
+              help="Output file name", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$output)){
+  print_help(opt_parser)
+  stop("An output file must be supplied\n", call.=FALSE)
+}
+
+q <- opt$q
+a <- opt$a_levels
+b <- opt$b_levels
+n <- opt$no_samples
+u <- opt$unbalance
+Cor <- opt$correlation_y
+Var <- opt$variance_y
+dd <- opt$DistDef
+lambda <- opt$lambda
+stdev <- opt$stdev
+loc <- opt$position
+pdist <- opt$p_dist
+hk <- opt$heterosk
+delta <- opt$delta
+transf <- opt$transf
+S <- round(opt$simulations)
+modelSim <- opt$model
+w <- opt$which
+output <- opt$output
+chunk <- opt$k_chunk
+cores <- opt$cpu
+
+
+## 1. Load packages and functions
+
+library(CompQuadForm)
+library(car)
+library(MCMCpack)
+library(MASS)
+library(plyr)
+library(copula)
+library(vegan)
+
+source("/users/rg/dgarrido/PhD/projects/sqtlseeker/paper/simulations/nf/bin/fx.R")
+
+## 2. Define parameters 
+
+labs <- label(a, b, n, u, w)
+A <- labs[[1]]
+B <- labs[[2]]
+
+if(w == "A"){
+  ch <- A
+} else if (w == "B") {
+  ch <- B
+} else if (w == "AB") {
+  ch <- A:B
+} else {
+  stop(sprintf("Unknown factor: '%s'.", w))
+}
+
+if(modelSim == "simplex"){
+  
+  tbl <- read.table(sprintf("/users/rg/dgarrido/PhD/projects/sqtlseeker/paper/simulations/nf/bin/qlocstdev.%s.tsv", pdist), h = T)
+  colnames(tbl) <- c("Q", "L", "S")
+  
+  if(! q %in% unique(tbl$Q)){
+    stop(sprintf("stdev not precomputed for q = %s", q))
+  } 
+  
+  stdev <- subset(tbl, Q == q & L == loc)$S
+  
+}
+
+## 3. Simulate
+if (chunk != 0) {
+  
+  tstats <- c()
+  set.seed(chunk)
+
+  for (i in 1:S){
+    
+    if (modelSim == "simplex") {
+      
+      if( i == 1){ # Sanity check
+        tol <- 0.01
+        check <- Sim.simplex(ch, q, n, loc, delta, hk, stdev, check = T, dist = pdist)
+        wm <- which.max(check$exp)
+        if( abs(check[wm, "obs"] - check[wm, "exp"]) > tol ) {
+          stop("Deviation from expected centroid greater than tolerance.")
+        }
+      }
+      
+      Y <- Sim.simplex(ch, q, n, loc, delta, hk, stdev, dist = pdist)
+      sd <- mean(apply(Y, 2, sd))
+      
+    } else if (modelSim == "mvnorm") {
+      
+      Y <- Sim.mvnorm(ch, q, n, mu = rep(0, q), delta, hk, Var, Cor)
+      
+    } else if (modelSim == "multinom") {
+      
+      N <- rpois(1, lambda)
+      Y <- Sim.multinom(ch, q, n, N, delta, loc)
+      
+    } else if (modelSim == "copula") {
+      
+      Y <- Sim.copula(ch, q, n, mu = rep(0, q), delta, hk, Var, Cor, dd)
+      
+    } else {
+      stop(sprintf("Unknown option: modelSim = '%s'.", modelSim))
+    }
+    
+    if (transf == "sqrt"){
+      Y <- sqrt(Y)
+    } else if (transf == "log"){
+      Y <- log(Y+1)
+    } else if (transf == "none"){
+      
+    } else {
+      stop(sprintf("Unknown option: transf = '%s'.", transf))
+    }
+    
+    # lm and residuals
+    Y <- scale(Y, center = T, scale = F)
+    fit <- lm(Y ~ A + B + A:B)
+    R <- fit$residuals
+    
+    # Sums of squares
+    UU <- Anova(fit, type = "II") # SS type II 
+    SS <- lapply(UU$SSP, function(x){sum(diag(x))})
+    SSe <- sum(diag(UU$SSPE))
+    
+    # Df
+    df.e <- fit$df.residual       # df.e <- (n-1) - sum(Df)
+    Df <- table(fit$assign)[-1]
+    names(Df) <- attributes(fit$terms)$term.labels
+    
+    # Statistic 
+    f <- unlist(lapply(SS, function(x){x/SSe})) 
+    # We divide by SSe for numerical stability when running CompQuadForm::davies
+    Fs <- f/Df*df.e 
+    
+    # Asymptotic test statistic (McArtor) 
+    G <- crossprod(Y)
+    eG <- eigen(G/n, only.values = T)$values # n or df.e ?
+    
+    # Asymptotic test statistic (Our proposal)
+    e <- eigen(cov(R)*(n-1)/df.e, symmetric = T, only.values = T)$values
+    
+    tstats <- rbind(tstats, c(Fs, unlist(SS), SSe, e, eG))
+
+  }
+  
+  write.table(tstats, file = output, sep = "\t", col.names = F, row.names = F, quote = F)
+  
+} else {
+  
+  set.seed(chunk)
+  
+  ######### This should be identical to the same piece of code above (except the check)
+  if (modelSim == "simplex") {
+    
+    tol <- 0.01
+    check <- Sim.simplex(ch, q, n, loc, delta, hk, stdev, check = T, dist = pdist)
+    wm <- which.max(check$exp)
+    if( abs(check[wm, "obs"] - check[wm, "exp"]) > tol ) {
+      stop("Deviation from expected centroid greater than tolerance.")
+    }
+    
+    Y <- Sim.simplex(ch, q, n, loc, delta, hk, stdev, dist = pdist)
+    sd <- mean(apply(Y, 2, sd))
+    
+  } else if (modelSim == "mvnorm") {
+    
+    Y <- Sim.mvnorm(ch, q, n, mu = rep(0, q), delta, hk, Var, Cor)
+    
+  } else if (modelSim == "multinom") {
+    
+    N <- rpois(1, lambda)
+    Y <- Sim.multinom(ch, q, n, N, delta, loc)
+    
+  } else if (modelSim == "copula") {
+    
+    Y <- Sim.copula(ch, q, n, mu = rep(0, q), delta, hk, Var, Cor, dd)
+    
+  } else {
+    stop(sprintf("Unknown option: modelSim = '%s'.", modelSim))
+  }
+  
+  if (transf == "sqrt"){
+    Y <- sqrt(Y)
+  } else if (transf == "log"){
+    Y <- log(Y+1)
+  } else if (transf == "none"){
+    
+  } else {
+    stop(sprintf("Unknown option: transf = '%s'.", transf))
+  }
+  
+  Y <- scale(Y, center = T, scale = F)
+  #########
+  d <- as.dist(interDist(Y))
+  
+  # Adonis permutations (raw data + type I SS)
+  # ado <- adonis(d ~ A + B + A:B, permutations = S, parallel = cores)
+
+  # save(ado, file = sprintf("%s.RData",output))
+  
+  # Adonis permutations (strata + type I SS)
+  perm <- how(nperm = S)
+  setBlocks(perm) <- ch
+  ado <- adonis(d ~ A + B + A:B, permutations = perm, parallel = cores) # WARNING: Adonis uses Type I SS, while we use Type II SS!
+                                                                         # Only valid to study A:B under H0
+  # Adonis permutations (strata + type II SS)
+  #ado <- list()
+  #ado$f.perms <- matrix(NA, nrow = 1, ncol = 7)
+  
+  #Yp <- Y
+  #for (p in 1:S){
+  #  print(p)
+  #  for (i in 1:b){Yp[B == i, ] <- Y[sample(which(B == i)),]}  
+  #  fit <- lm(Yp ~ A + B + A:B)
+  #  R <- fit$residuals
+  #  UU <- Anova(fit, type = "II") # SS type II 
+  #  SS <- lapply(UU$SSP, function(x){sum(diag(x))})
+  #  SSe <- sum(diag(UU$SSPE))
+  #  df.e <- fit$df.residual      
+  #  Df <- table(fit$assign)[-1]
+  #  f <- unlist(lapply(SS, function(x){x/SSe})) 
+  #  ado$f.perms <- rbind(ado$f.perms, cbind(t(f/Df*df.e), t(as.numeric(SS)), t(SSe)))
+  #}
+  #ado$f.perms <- ado$f.perms[-1,]
+  save(ado, file = sprintf("%s.RData",output))
+  
+}
